1611663887c4ad1f4da5a15353d546659ab6c334
Todd Kridel <todd.kridel@gmail.com>
2023-01-03 02:15:26 -0700

    Inquirer prompt sequences and logical flow for program stages

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 
 # Bootcamp Module 10 Challenge Assignment -- TK Team Employee Information Profile Webpage Generator
 
-![a license icon](.//images/License_Badge__The_MIT_License.jpg)
+![a license icon](./assets/images/License_Badge__The_MIT_License.jpg)
 
 
 --------------------------------
@@ -31,17 +31,23 @@ This current Module 10 Challenge assignment -- Team Employee Information Profile
 
 The application is a command-line application that accepts user input that use. The application uses the npm Inquirer module to prompt to the user for the information that is necessary for processing.
 
+
 ----
+
 
 The objectives and operational features -- user story and acceptance criteria items -- of the assignment application were as follows:
 
+|
 [ User Story ]
+|
 
 AS A manager...
 I WANT to generate a webpage that displays my team's basic info...
 SO THAT I have quick access to their emails and GitHub profiles.
 
+|
 [ Acceptance Criteria ]
+|
 
 WHEN I am prompted for my team members and their information...
 THEN an HTML file is generated that displays a nicely formatted team roster based on user input. The HTML webpage is titled “My Team” and contains for each entered employee a card div display boxes that lists the name, title/role, and other role-specific primary property information.
@@ -67,7 +73,9 @@ THEN I am prompted to enter the intern’s name, ID, email, and school, and I am
 WHEN I decide to finish building my team...
 THEN I exit the application, and the HTML is generated,
 
+|
 [ Additional Grading Criteria ]
+|
 
 Also good coding practices and good GitHub/Git repository configuration.
 
@@ -75,37 +83,47 @@ Also: The application includes a usage-demonstration walk-through video. The vid
 
 ![an link to the application usage demo video](./assets/images/)
 
+|
 [ Application Components ]
+|
 
 The application has the following primary directory structure:
 
-.                          // base/parent directory
-├──___tests___/            // jest tests
-│   ├── Employee.test.js
-│   ├── Engineer.test.js
-│   ├── Intern.test.js
-│   └── Manager.test.js
-├── dist/                  // rendered output (HTML) and CSS style sheet      
-├── lib/                   // classes
-├── src/                   // template helper code 
-├── .gitignore             // indicates which folders and files Git should ignore
-├── index.js               // runs the application
-└── package.json    
+    .                          // base/parent directory
+    |
+    ├──__ tests __/            // jest tests
+    |   |
+    │   ├── Employee.test.js
+    |   |
+    │   ├── Engineer.test.js
+    |   |
+    │   ├── Intern.test.js
+    |   |
+    │   └── Manager.test.js
+    |
+    ├── dist/                  // rendered output (HTML) and CSS style sheet 
+    |
+    ├── lib/                   // classes
+    |
+    ├── src/                   // template helper code 
+    |
+    ├── .gitignore             // indicates which folders and files Git should ignore
+    |
+    ├── index.js               // runs the application
+    |
+    └── package.json    
 
+Note: The "dist" sub-folder is removed from the .gitignore file so that the Git process tracks the folder and includes it when updated files are pushed up to the application repository.
 
-Note: The "dist" sub-folder is removed from the .gitignore file so that the Git process 
-tracks the folder and includes it when updated files are pushed up to the application 
-repository.
+The "__ tests __" directory contains the functionality tests that are for the application classes and all of the tests pass.
 
-The "_tests_" directory contains the functionality tests that are for the application 
-classes and all of the tests pass.
-
+|
 [ Data Storage Classes/Objects ]
+|
 
-The application has the following classes: Employee (parent class), Manager (sub-class), 
-Engineer (sub-class), and Intern (sub-class). The 3 sub-classes extend the parent class.
+The application has the following classes: Employee (parent class), Manager (sub-class), Engineer (sub-class), and Intern (sub-class). The 3 sub-classes extend the parent class.
 
->> The Employee class is the parent class that has the following properties and methods that are common to and shared by the other classes that are related sub-classes.
+The Employee class is the parent class that has the following properties and methods that are common to and shared by the other classes that are related sub-classes.
 
 * name
 * id
@@ -115,18 +133,18 @@ Engineer (sub-class), and Intern (sub-class). The 3 sub-classes extend the paren
 * getEmail()
 * getRole()  // job title
 
->> The Manager sub-class has the following role-specific data properties and methods in addition to the inherited common properties and methods of the Employee parent class.
+ The Manager sub-class has the following role-specific data properties and methods in addition to the inherited common properties and methods of the Employee parent class.
 
 * officeNumber
 * getRole()
 
->> The Engineer sub-class has the following role-specific data properties and methods in addition to the inherited common properties and methods of the Employee parent class.
+The Engineer sub-class has the following role-specific data properties and methods in addition to the inherited common properties and methods of the Employee parent class.
 
 * github  // GitHub username
 * getGithub()
 * getRole()  // job title
 
->> The Intern sub-class has the following role-specific data properties and methods in addition to the inherited common properties and methods of the Employee parent class.
+The Intern sub-class has the following role-specific data properties and methods in addition to the inherited common properties and methods of the Employee parent class.
 
 * school
 * getSchool()
@@ -137,9 +155,33 @@ Engineer (sub-class), and Intern (sub-class). The 3 sub-classes extend the paren
 
 Make sure that you have a GitHub membership and account to be able to view the repository of the assignment webpage system. The published GitHub Pages view of the website should be accessible on the public internet access URL without a need to have a GitHub membership/account.
 
-IMPORTANT/REQUIRED: After when the repository has been cloned/downloaded to a local computer directory from the main GitHub location...then type the node npm command "npm i" in the clone directory to trigger an auto-installation of the additional npm module sub-programs that are required for this application to be able to function. Or if that auto-install does not occur...then the additional modules can be individually manually installed by using the "npm install < module name > < version number >" command in a console window of the clone directory.
+IMPORTANT/REQUIRED: After when the repository has been cloned/downloaded to a local computer directory from the main GitHub location...make sure that the node.js system software and the npm (node package manager) system software is installed...and then type the node npm command "npm i" in the clone directory to trigger an auto-installation of the additional npm module sub-programs that are required for this application to be able to function. Or if that auto-install does not occur...then the additional modules can be individually manually installed by using the "npm install < module name > < version number >" command in a console window of the clone directory. Refer to the appropriate system website documentation for additional information.
 
-DEPENDENCY INFORMATION OF THE "PACKAGE.JSON" FILE
+    DEPENDENCY INFORMATION OF THE "PACKAGE.JSON" FILE
+
+    "name": "tk-team-employee-information-profile-webpage-generator",
+    "version": "1.0.0",
+    "description": "This program is a console application that uses several Inquirer prompt sequences to gather from/for a team leader/manager some general employee profile information that is applicable to their team; and then the program upon exit of the prompt process generates a dynamic HTML webpage that shows a graphical view of the team employee information profile.",
+    "main": "index.js", 
+    "directories": {
+        "lib": "lib"
+    },
+    ----
+    "dependencies": {
+        "fs": "current version",
+        "inquirer": "8.2.4",
+        "open": "current version"
+    },
+    ----
+    "devDependencies": {
+        "jest": "^29.3.1"
+    },
+    "scripts": {
+        "test": "jest", 
+        "start": "index.js" 
+    },
+    "author": "Todd Kridel",
+    "license": "MIT"
 
 
 ## 3. Program Usage Information
@@ -170,16 +212,21 @@ The application uses the npm Jest module for running the code unit functional te
 
 SUCCESSFULLY TESTED -- ALL UNITS (as shown in the following image and included in the usage demo video)
 
+![a view of the successful passing automated Jest functional tests](./assets/images/All%20Class%20Unit%20Tests%20Passed.png)
+
 
 ## 6. Program Questions
 
 Email Address: todd.kridel@gmail.com
-GitHub User Name: Todd-Kridel
+
+GitHub User Name: https://github.com/Todd-Kridel
 
 
 ## 7. Program License Information
 
-MIT License
+MIT License   
+
+![a license icon](./assets/images/License_Badge__The_MIT_License.jpg)
 
 Copyright (c) 2022 DU Full-Stack Web Development Coding Boot Camp
 
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 // Include packages that are needed for the application.
 
 const theInquirerObject = require("inquirer");
@@ -23,11 +25,291 @@ let theHTMLTextContent;
 let newEmployeeObject;
 
 
+// Create arrays of the details of all of the questions that are to be asked for the creation 
+// of the team profile page/file.
+
+//
+// the Inquirer questions/arrays
+// /////////////////////////////
+//
+// ( The start of the application. )
+// [ managerInformationQuestions ]
+// "What is the manager's name?"; managerName
+// "What is the manager's employee ID?"; managerEmployeeId
+// "What is the manager's email address?"; managerEmailAddress
+// "What is the manager's office number?"; managerOfficeNumber
+//
+// [ informationEntryMenuAction ] <= [ informationEntryMenuOptions ]
+// "What do you now want to do? possible options:"; managerEntryOptions
+//   "Enter engineer employee information."; engineerEntryOptionSelection
+//   "Enter intern employee information."; internEntryOptionSelection
+//   "Finish the team profile entry."; finishEntryOptionSelection
+//     ( Exit the prompt application and generate the team profile HTML page/file. )
+//
+// [ engineerInformationEntryQuestions ]
+// "What is the engineer's name?"; engineerName
+// "What is the engineer's employee ID?"; engineerEmployeeId
+// "What is the engineer's email address?"; engineerEmailAddress
+// "What is the engineer's GitHub username?"; engineerGitHubUserName
+// ( Return to Entry Options menu. )
+//
+// [ internInformationEntryQuestions ]
+// "What is the intern's name?"; internName
+// "What is the intern's employee ID?"; internEmployeeId
+// "What is the intern's email address?"; internEmailAddress
+// "What is the intern's school?"; internSchool
+// ( Return to Entry Options menu. )
+//
+// /////////////////////////////
+// 
+// program logic/process flow:
+// if new session => display manager questions
+// if done with manager questions => display manager entry options
+//   if engineer option is selected => display engineer questions
+//     if done with engineer questions => display manager entry options
+//   if intern option is selected => display intern questions
+//     if done with intern questions => display manager entry options
+//   if finish option is selected => exit the question process and generate and display HTML
+//
+//
+const managerInformationQuestions = 
+[
+{
+name: "name", 
+type: "input", 
+message: "What is the manager's name?"
+}, 
+{
+name: "id", 
+type: "input", 
+message: "What is the manager's employee ID?"
+}, 
+{
+name: "emailAddress", 
+type: "input", 
+message: "What is the manager's email address?"
+}, 
+{
+name: "officeNumber", 
+type: "input", 
+message: "What is the manager's office number?"
+}
+// ( Go to Entry Options menu. )
+];
+//
+//
+const engineerInformationEntryQuestions = 
+[
+{
+name: "name", 
+type: "input", 
+message: "What is the engineer's name?"
+}, 
+{
+name: "id", 
+type: "input", 
+message: "What is the engineer's employee ID?"
+}, 
+{
+name: "emailAddress", 
+type: "input", 
+message: "What is the engineer's email address?"
+}, 
+{
+name: "githubUserName", 
+type: "input", 
+message: "What is the engineer's GitHub username?"
+}
+// ( Return to Entry Options menu. )
+];
+//
+//
+const internInformationEntryQuestions = 
+[
+{
+name: "name", 
+type: "input", 
+message: "What is the intern's name?"
+}, 
+{
+name: "id", 
+type: "input", 
+message: "What is the intern's employee ID?"
+}, 
+{
+name: "emailAddress", 
+type: "input", 
+message: "What is the intern's email address?"
+}, 
+{
+name: "school", 
+type: "input", 
+message: "What is the intern's school?"
+}
+// ( Return to Entry Options menu. )
+];
+//
+//
+const informationEntryMenuOptions = 
+  // => informationEntryMenuAction of [ informationEntryOptionMenuQuestions]
+[
+"Enter engineer employee information.", 
+"Enter intern employee information.", 
+"Finish the team profile entry."
+];
+//
+//
+const informationEntryOptionMenuQuestions = 
+[
+{
+name: "informationEntryMenuAction",  //  
+type: "list", 
+message: "What do you now want to do? possible options:", 
+choices: informationEntryMenuOptions
+}
+];
 
 
-// ???
+// Create a function that will get from the manager the manager information that is necessary 
+// for processing of the team profile.
+//
+function doGetManagerProfileInformationFromTheManager() {
+// Issue a call to the prompt function of the inquirer object to get manager information for 
+// team profile file.
+theInquirerObject
+.prompt(managerInformationQuestions)
+.then((answers) => {
+  //console.log(answers);
+  //console.log(answers.name + " " + answers.id + " " + answers.emailAddress + " " + 
+  //  answers.officeNumber);
+  newEmployeeObject = new Manager(answers.name, answers.id, answers.emailAddress, 
+    answers.officeNumber);
+  // Call to the utility function that generates the ongoing compilation of the team profile 
+  // dynamic HTML file text based on the ongoing user input.
+  theEmployeeDynamicHTMLContentTextProcessing = theEmployeeDynamicHTMLContentTextProcessing + 
+    doGenerateDynamicHTMLTextContent(newEmployeeObject);
+  console.log("Added Manager: " + newEmployeeObject.name);
+  //console.log(theEmployeeDynamicHTMLContentTextProcessing);
+  doGetInformationEntryActionsFromTheManager(); 
+  //
+})
+.catch((error) => {
+  if (error.isTtyError) {
+    // Prompt could not be rendered in the current environment.
+  }
+  else {
+    // A problem occurred with the utility-function processing of the prompt answer data.
+    console.log("ERROR: A problem occurred with the processing of the prompt answer data.");
+  };
+});
+}
 
 
+// Create a function that will get from the manager the next desired information entry action 
+// that is necessary for processing of the team profile.
+//
+function doGetInformationEntryActionsFromTheManager() {
+// Issue a call to the prompt function of the inquirer object to get manager information for 
+// team profile file.
+theInquirerObject
+.prompt(informationEntryOptionMenuQuestions)
+.then((answer) => {
+  console.log(answer.informationEntryMenuAction);
+  // Call to the utility function that generates the ongoing compilation of the team profile 
+  // dynamic HTML file text based on the ongoing user input.
+  switch (answer.informationEntryMenuAction) { 
+    //
+    case "Enter engineer employee information.": 
+      doGetEngineerProfileInformationFromTheManager();
+      break;  
+    case "Enter intern employee information.": 
+      doGetInternProfileInformationFromTheManager();
+      break;
+    case "Finish the team profile entry.":
+      // Call to the utility function that finalizes the output HTML text of the team employee 
+      // profile. 
+      doFinalizeTheHTMLTextContentOfTheTeamEmployeeProfile();
+      //
+      // Open the generated HTML file/URL in the default browser.
+      theURLOpenObject("./dist/index.html");
+      console.log("EXIT THE APPLICATION");
+      break;
+  }; 
+//
+})
+.catch((error) => {
+  if (error.isTtyError) {
+    // Prompt could not be rendered in the current environment.
+  }
+  else {
+    // A problem occurred with the utility-function processing of the prompt answer data.
+    console.log("ERROR: A problem occurred with the processing of the prompt answer data.");
+  };
+});
+//
+}
+//
+//
+// Create a function that will get from the manager the engineer information that is necessary 
+// for processing of the team profile.
+//
+function doGetEngineerProfileInformationFromTheManager() {
+// Issue a call to the prompt function of the inquirer object to get information for the 
+// team profile file.
+theInquirerObject
+.prompt(engineerInformationEntryQuestions)
+.then((answers) => {
+  newEmployeeObject = new Engineer(answers.name, answers.id, answers.emailAddress, 
+    answers.githubUserName);
+  // Call to the utility function that generates the ongoing compilation of the team profile 
+  // dynamic HTML file text based on the ongoing user input.
+  theEmployeeDynamicHTMLContentTextProcessing = theEmployeeDynamicHTMLContentTextProcessing + 
+    doGenerateDynamicHTMLTextContent(newEmployeeObject);
+  console.log("Added Engineer: " + newEmployeeObject.name);
+  doGetInformationEntryActionsFromTheManager();
+  //
+})
+.catch((error) => {
+  if (error.isTtyError) {
+    // Prompt could not be rendered in the current environment.
+  } 
+  else {
+    // A problem occurred with the utility-function processing of the prompt answer data.
+    console.log("ERROR: A problem occurred with the processing of the prompt answer data.");
+  };
+});
+}
+//
+//
+// Create a function that will get from the manager the intern information that is necessary 
+// for processing of the team profile.
+//
+function doGetInternProfileInformationFromTheManager() {
+// Issue a call to the prompt function of the inquirer object to get information for the 
+// team profile file.
+theInquirerObject
+.prompt(internInformationEntryQuestions)
+.then((answers) => {
+  newEmployeeObject = new Intern(answers.name, answers.id, answers.emailAddress, 
+    answers.school);
+  // Call to the utility function that generates the ongoing compilation of the team profile 
+  // dynamic HTML file text based on the ongoing user input.
+  theEmployeeDynamicHTMLContentTextProcessing = theEmployeeDynamicHTMLContentTextProcessing + 
+    doGenerateDynamicHTMLTextContent(newEmployeeObject);
+  console.log("Added Intern: " + newEmployeeObject.name);
+  doGetInformationEntryActionsFromTheManager();
+  //
+})
+.catch((error) => {
+  if (error.isTtyError) {
+    // Prompt could not be rendered in the current environment.
+  }
+  else {
+    // A problem occurred with the utility-function processing of the prompt answer data.
+    console.log("ERROR: A problem occurred with the processing of the prompt answer data.");
+  };
+});
+}
 
 
 // Create a function that writes the final compiled dynamic team employee profile data to the 
@@ -44,7 +326,7 @@ theHTMLTextContent = doGenerateFinalHTMLTextContent(theEmployeeDynamicHTMLConten
 //console.log(theHTMLTextContent);
 theFileSystemObject.writeFileSync("./dist/index.html", theHTMLTextContent);
 console.log("SOME FILE WRITE PROCESSING OCCURRED: Created file 'index.html' " + 
-    "and saving the inputted information.");
+  "and saving the inputted information.");
 console.log(theHTMLTextContent);
 }
 
